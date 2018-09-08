00f7d509cb784e3b46760ef4f08365e6b2e6cb97
shihaitao123 <761433102@qq.com>
2018-09-08 18:41:04 +0800

    Site updated: 2018-09-08 18:41:03

--- dist/music.js
+++ dist/music.js
@@ -3,6 +3,18 @@ const ap = new APlayer({
     fixed: true,
     autoplay: true,
     audio: [
+	  {
+        name: '阿珍爱上了阿强',
+        artist: '五条人',
+        url: 'http://www.ytmp3.cn/down/52596.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/97.jpg',
+      },
+	   {
+        name: '信仰',
+        artist: '张信哲',
+        url: 'http://www.ytmp3.cn/down/52598.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/56.jpg',
+      },
       {
         name: '盛夏的果实',
         artist: '莫文蔚',
@@ -12,20 +24,14 @@ const ap = new APlayer({
       {
         name: '再见',
         artist: '张震岳',
-        url: 'http://www.ytmp3.cn/down/52596.mp3',
+        url: 'http://www.ytmp3.cn/down/52597.mp3',
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/2.jpg',
       },
       {
-        name: '阿珍爱上了阿强',
-        artist: '五条人',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/97.jpg',
-      },
-	  {
-        name: '信仰',
-        artist: '张信哲',
-        url: 'http://www.ytmp3.cn/down/52598.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/56.jpg',
+        name: '无论你多怪异我还是会喜欢你',
+        artist: '江惠莲',
+        url: 'http://www.ytmp3.cn/down/51590.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/61.jpg',
       }
     ]
 });
