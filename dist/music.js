f71308022720389ef55731559b43d642fc017d1d
shihaitao123 <761433102@qq.com>
2018-09-08 18:15:10 +0800

    Site updated: 2018-09-08 18:15:10

---
+++ dist/music.js
@@ -0,0 +1,25 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "ʢ�ĵĹ�ʵ",
+        artist: 'Ī��ε',
+        url: 'http://www.ytmp3.cn/down/32464.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '׽����',
+        artist: '׿����',
+        url: 'http://www.ytmp3.cn/down/46964.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/92.jpg',
+      },
+      {
+        name: '������',
+        artist: '������',
+        url: 'http://up.mcyt.net/?down/46644.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      }
+    ]
+});
