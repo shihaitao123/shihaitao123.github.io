a4dbe259e800a5c177450f6b13434f4f0d3cb541
shihaitao123 <761433102@qq.com>
2018-09-08 18:32:29 +0800

    Site updated: 2018-09-08 18:32:29

--- dist/music.js
+++ dist/music.js
@@ -1,25 +1,31 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     audio: [
       {
-        name: "ʢ�ĵĹ�ʵ",
-        artist: 'Ī��ε',
+        name: '盛夏的果实',
+        artist: '莫文蔚',
         url: 'http://www.ytmp3.cn/down/32464.mp3',
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
       },
       {
-        name: '׽����',
-        artist: '׿����',
-        url: 'http://www.ytmp3.cn/down/46964.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/92.jpg',
+        name: '再见',
+        artist: '张震岳',
+        url: 'http://www.ytmp3.cn/down/52596.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/2.jpg',
       },
       {
-        name: '������',
-        artist: '������',
+        name: '阿珍爱上了阿强',
+        artist: '五条人',
         url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/97.jpg',
+      },
+	  {
+        name: '信仰',
+        artist: '张信哲',
+        url: 'http://www.ytmp3.cn/down/52598.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/56.jpg',
       }
     ]
 });
