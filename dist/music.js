666fcfc2d9e5315c9dd46f257eeb90585a06ab2d
shihaitao123 <761433102@qq.com>
2018-09-16 20:34:05 +0800

    Site updated: 2018-09-16 20:34:04

--- dist/music.js
+++ dist/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: true,
+    autoplay: false,
     audio: [
 	  {
         name: '阿珍爱上了阿强',
